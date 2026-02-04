644accbabf6b3d78f30946f719d142396d41062c
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2026-02-04 17:08:26 +0000

    Add: constants core

---
+++ Proyect-trivia/mvp/lib/core/constants.dart
@@ -0,0 +1,11 @@
+const double kDouble5 = 5.0;
+const double kDouble10 = 10.0;
+const double kDouble20 = 20.0;
+const double kDouble40 = 40.0;
+
+const Map<String, String> themeImages = {
+  'Cultura': 'images/cultura.png',
+  'Ciencias': 'images/ciencias.png',
+  'Actualidad': 'images/actualidad.png',
+  'Hobbies': 'images/hobbies.png',
+};
