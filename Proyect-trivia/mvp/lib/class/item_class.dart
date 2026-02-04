d9c8e7bf3ac723ff959977530c5fdfb8f1378b73
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2026-02-04 17:07:29 +0000

    Add: item class

---
+++ Proyect-trivia/mvp/lib/class/item_class.dart
@@ -0,0 +1,17 @@
+import 'dart:math';
+
+class ItemClass {
+  final String title;
+  final String tema;
+
+  static const List<String> temas = [
+    'Cultura',
+    'Ciencias',
+    'Actualidad',
+    'Hobbies',
+  ];
+
+  ItemClass({required int nivel})
+    : title = 'Nivel $nivel',
+      tema = temas[Random().nextInt(temas.length)];
+}
