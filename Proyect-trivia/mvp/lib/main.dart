260422a90837bb7c19c1aea14e809d0643a3b98b
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2026-02-04 14:01:48 +0000

    Change: remplace all main

--- Proyect-trivia/mvp/lib/main.dart
+++ Proyect-trivia/mvp/lib/main.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:Proyect-trivia/mvp/pages/home.dart';
+import 'package:Proyect-trivia/mvp/pages/map.dart';
 
-void main() {
-  runApp(const TriviaApp());
-}
-
-class TriviaApp extends StatelessWidget {
-  const TriviaApp({super.key});
-
-  
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Trivia Start',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.green)),
-      home: Text('This is a trivia game'),
-    );
-  }
-}
+void main() => runApp(MaterialApp(
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => Home(),
+        '/map': (context) => MapPage(),
+      },
+    )); // MaterialApp
