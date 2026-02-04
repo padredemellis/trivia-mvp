1a72668dc8eae0a24e5b41a941b13e170471217e
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2026-02-04 17:12:15 +0000

    Fix: name map

--- Proyect-trivia/mvp/lib/main.dart
+++ Proyect-trivia/mvp/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'mvp/pages/home.dart';
-import 'mvp/pages/map.dart';
+import 'package:tu_app/pages/home.dart';
+import 'package:tu_app/pages/map.dart';
 
 void main() => runApp(MaterialApp(
       initialRoute: '/home',
       routes: {
         '/home': (context) => Home(),
-        '/map': (context) => MapPage(),
+        '/map': (context) => HomePage(),
       },
     )); // MaterialApp
