c781e48355d300494b356d4113b5e0c81516f3dd
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2026-02-05 16:43:28 +0000

    Change: card_widget by node_button

---
+++ Proyect-trivia/mvp/lib/widget/node_button.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:mvp/models/node.dart';
+import 'package:mvp/core/constants.dart';
+import 'package:mvp/pages/quiz.dart';
+
+class NodeButton extends StatelessWidget {
+  const NodeButton({super.key, required this.box});
+  final Node box;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => QuizPage(box: box)),
+        );
+      },
+      child: Column(
+        children: [
+          Container(
+            width: 70,
+            height: 70,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                ),
+              ],
+            ),
+            child: ClipOval(
+              child: Image.asset(
+                themeImages[box.tema] ?? 'images/default.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(height: kDouble5),
+          Text(
+            box.title,
+            style: const TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(box.tema, style: const TextStyle(fontSize: 14.0)),
+        ],
+      ),
+    );
+  }
+}
