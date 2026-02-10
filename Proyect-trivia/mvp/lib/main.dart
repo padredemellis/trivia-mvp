f40b9a04cc6546c6a3a496d48b16a34330cedc1d
HB10065 <10065@holbertonstudents.com>
2026-02-10 11:03:22 -0800

    Firebase implemented and initialized in main.dart

--- Proyect-trivia/mvp/lib/main.dart
+++ Proyect-trivia/mvp/lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:mvp/pages/home.dart';
 import 'package:mvp/pages/map.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // set up Flutter Engine
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); // intialize Database
+  
+  runApp(
   MaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: '/home',
     routes: {'/home': (context) => Home(), '/map': (context) => HomePage()},
   ),
 ); // MaterialApp
+}
