cf51c52a4d03f300a640bc0cd1d95a3d3aa231af
sofia1098 <sofianunez000@gmail.com>
2026-02-12 21:04:52 -0300

    Arreglo imports y agrego constantes

---
+++ Proyect-trivia/mvp/lib/core/constants/app_color.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColor {
+  static const Color backgroundVerde = Color.fromARGB(255, 138, 201, 66);
+  static const Color backgroundCrema = Color.fromARGB(255, 218, 185, 136);
+  static const Color oscuro = Color.fromARGB(255, 121, 68, 28);
+  static const Color titulo = Color.fromARGB(255, 254, 111, 79);
+
+}
---
+++ Proyect-trivia/mvp/lib/core/constants/text_styles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:mvp/core/constants/app_color.dart';
+
+class TextStyles {
+  static const TextStyle respuesta = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: AppColor.oscuro,
+  );
+  static const TextStyle pregunta = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    color: AppColor.oscuro,
+  );
+}
